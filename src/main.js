a89128ed5ed7b40b5771da89155a00fb3a547e1f
Eric Bermudez <eric.bermudez@appetiser.com.au>
2020-05-26 15:11:25 +0800

    Import vue-router

--- src/main.js
+++ src/main.js
@@ -1,9 +1,47 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from '@/App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-Vue.config.productionTip = false
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+Vue.config.productionTip = false;
+
+import Home from '@/components/Home.vue';
+import Create from '@/components/Create.vue';
+import Index from '@/components/Index.vue';
+import Edit from '@/components/Edit.vue';
+
+const routes = [
+  {
+      name: 'home',
+      path: '/',
+      component: Home
+  },
+  {
+      name: 'create',
+      path: '/create',
+      component: Create
+  },
+  {
+      name: 'posts',
+      path: '/posts',
+      component: Index
+  },
+  {
+      name: 'edit',
+      path: '/edit/:id',
+      component: Edit
+  }
+];
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: routes
+});
+
+new Vue(
+  Vue.util.extend({
+    router 
+  }, App)
+).$mount('#app');
